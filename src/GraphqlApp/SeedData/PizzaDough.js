ae37fbad3d68270e1794ee39c735fc7499289903
Andrew Pecha <andrew.pecha@gmail.com>
2022-01-15 12:12:21 -0600

    add pizza dough seed data

---
+++ src/GraphqlApp/SeedData/PizzaDough.js
@@ -0,0 +1,34 @@
+[
+    {
+        "recipeName": "HG",
+        "mixTimeInMinutes": 1.5,
+        "ingredients": [
+            {
+                "ingredientName": "water",
+                "amount": 100,
+                "uom": "fl oz"
+            },
+            {
+                "ingredientName": "flour",
+                "amount": 50,
+                "uom": "lb"
+            }
+        ]
+    },
+    {
+        "recipeName": "SAV",
+        "mixTimeInMinutes": 3.5,
+        "ingredients": [
+            {
+                "ingredientName": "water",
+                "amount": 60,
+                "uom": "fl oz"
+            },
+            {
+                "ingredientName": "flour",
+                "amount": 35,
+                "uom": "lb"
+            }
+        ]
+    }
+]
