8456bcb7f9d77d123b1e1479ad465e8659a4e952
Andrew Pecha <andrew.pecha@gmail.com>
2022-01-16 11:38:22 -0600

    map mongo object id behind string type for PizzaDough model

--- src/GraphqlApp/Models/PizzaDough.cs
+++ src/GraphqlApp/Models/PizzaDough.cs
@@ -1,10 +1,13 @@
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace GraphqlApp.Models;
 
 public class PizzaDough
 {
-    public ObjectId _id { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
     public string RecipeName { get; set; }
     public decimal MixTimeInMinutes { get; set; }
     public Ingredient[] Ingredients { get; set; }
